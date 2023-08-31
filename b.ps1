c103ecf2a17f9f9d931fbe935c05acd681bf4084
DoganM95 <doganmermertas@hotmail.de>
2023-09-01 01:35:13 +0200

    bind inout && output volumes

--- b.ps1
+++ b.ps1
@@ -2,4 +2,4 @@ docker rm dc
 docker rmi dc 
 
 docker build -t dc .
-docker run --rm --name dc -p 5800:5800 -p 5900:5900 -v ".\volume\data:/config/xdg/data" -v ".\volume\config:/config/xdg/config" dc
+docker run --rm --name dc -p 5800:5800 -p 5900:5900 -v ".\volume\data:/config/xdg/data" -v ".\volume\config:/config/xdg/config" -v ".\volume\input:/app/input" -v ".\volume\output:/app/output" dc
