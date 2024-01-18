4e68682301983145250d9315f9ae53c0e59a1f42
DoganM95 <doganmermertas@hotmail.de>
2024-01-18 21:03:11 +0100

    implement settings keeper (renames folder to current version)

--- startapp.sh
+++ startapp.sh
@@ -1,5 +1,28 @@
 #!/bin/sh
 
+# Export cura version to env
+export CURRENT_CURA_VERSION=$(cat /app/current_cura_version.txt)
+
+# Extract major_minor part to use as folder name
+CURRENT_CURA_VERSION_MAJOR_MINOR=$(echo $CURRENT_CURA_VERSION | cut -d'.' -f1,2)
+
+# Fix permissions (just in case)
+chmod -R 777 /config/xdg/*
+
+# Print current version
+echo "current cura version: $CURRENT_CURA_VERSION"
+echo "current cura major_minor version: $CURRENT_CURA_VERSION_MAJOR_MINOR"
+
+# Rename previous versions folders to match current version, to keep settings and plugins (stupid cura)
+if [ ! $(find /config/xdg/data/cura -maxdepth 0 -empty) ]; then
+    mv /config/xdg/data/cura/* /config/xdg/data/cura/$CURRENT_CURA_VERSION_MAJOR_MINOR
+    echo 
+fi
+
+if [ ! $(find /config/xdg/config/cura -maxdepth 0 -empty) ]; then
+    mv /config/xdg/config/cura/* /config/xdg/config/cura/$CURRENT_CURA_VERSION_MAJOR_MINOR
+fi
+
 # Start openbox
 openbox &
 
